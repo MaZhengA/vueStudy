9f620def73fc2c03d3c659c41d05953afa4d8202
MaZhengA <17826817363@163.com>
2023-04-06 14:19:50 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -8,22 +8,30 @@ import Second from '@/pages/Second.vue';
 import Detail from '@/pages/Detail.vue';
 
 
-export default new VueRouter({
+const router = new VueRouter({
   routes: [
     {
       path: '/home',
       component: Home,
+      // meta: {isAuth: true, title: '主页'}, // meta配置元信息
       children: [
         // 当 /home 匹配成功，
         // First 会被渲染在 Home 的 <router-view> 中
         { 
           path: 'first', 
           component: First,
+          meta: {isAuth: true, title: '第一个家'},
+          // beforeEnter: (to, from, next) => {
+          //   // 单个路由守卫
+          //   next()
+          //   console.log(to, from)
+          // }
         },
         // ...其他子路由
         { 
           path: 'second', 
           component: Second,
+          meta: {isAuth: false},
           children: [
             { 
               name: 'xiangqing',
@@ -44,7 +52,30 @@ export default new VueRouter({
     },
     {
       path: '/about',
-      component: About
+      component: About,
+      // meta: {isAuth: true, title: '关于'}
     },
   ]
-})
+});
+
+// 全局前置路由守卫，当一个导航触发时，全局前置守卫按照创建顺序调用
+// router.beforeEach((to, from, next) => {
+  // to:即将要进入的目标 路由对象
+  // from:当前导航正要离开的路由
+  // next:执行下一段操作
+  // console.log('执行',to, from)
+//   if (to.meta.isAuth) {
+//     if (localStorage.getItem('name') === 'mazheng') {
+//       next()
+//     }
+//   } else {
+//     alert('看不了啊兄弟')
+//   }
+// });
+
+// 后置路由守卫，可以用来修改网页标题
+// router.afterEach((to, from) => {
+//   document.title = to.meta.title; // 修改网页标题
+// })
+
+export default router;
